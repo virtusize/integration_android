a0f2999be4f80bbb02a96a35cdbb7b75cc7b91e3
Kuei-Jung Hu <akueisara39@gmail.com>
2020-07-17 11:45:54 +0900

    Add comments for initializing the Virtusize instance

--- sampleAppKotlin/src/main/java/com/virtusize/android/App.kt
+++ sampleAppKotlin/src/main/java/com/virtusize/android/App.kt
@@ -13,12 +13,20 @@ class App: Application() {
 
         // Initialize Virtusize instance for your application
         Virtusize = VirtusizeBuilder().init(this)
+            // Only the API key is required
             .setApiKey("15cc36e1d7dad62b8e11722ce1a245cb6c5e6692")
+            // For using the Order API, a user ID is required
             .setUserId("123")
+            // By default, the Virtusize environment will be set to GLOBAL
             .setEnv(VirtusizeEnvironment.STAGING)
+            // By default, the initial language will be set based on the Virtusize environment
             .setLanguage(VirtusizeLanguage.EN)
-            .setShowSGI(false)
+            // By default, ShowSGI is false
+            .setShowSGI(true)
+            // By default, Aoyama allows all the possible languages including English, Japanese and Korean
             .setAllowedLanguages(mutableListOf(VirtusizeLanguage.EN, VirtusizeLanguage.JP))
+            // By default, Aoyama displays all the possible info categories in the Product Details tab,
+            // including "modelInfo", "generalFit", "brandSizing" and "material".
             .setDetailsPanelCards(mutableListOf(VirtusizeInfoCategory.BRAND_SIZING, VirtusizeInfoCategory.GENERAL_FIT))
             .build()
     }
