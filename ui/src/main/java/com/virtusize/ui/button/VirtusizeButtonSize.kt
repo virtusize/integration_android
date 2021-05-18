9641f734a4b5050ad2fc54ffe44581254b9637b5
Kuei-Jung Hu <kueijunghu@gmail.com>
2021-05-18 17:51:25 +0900

    Rearrange the positions of the enum constants of VirtusizeButtonSize and VirtusizeButtonTextSize

--- ui/src/main/java/com/virtusize/ui/button/VirtusizeButtonSize.kt
+++ ui/src/main/java/com/virtusize/ui/button/VirtusizeButtonSize.kt
@@ -4,5 +4,6 @@ package com.virtusize.ui.button
  * This enum contains the sizes that can be applied to the Virtusize Button
  */
 enum class VirtusizeButtonSize {
-    STANDARD, SMALL
+    STANDARD,
+    SMALL
 }
