a7f0bdc5681796e3b545f316babb7097a2c4e13e
Kuei-Jung Hu <kueijunghu@gmail.com>
2021-06-08 18:30:23 +0900

    Make sure users can still get the WebChromeClient callbacks when replacing Webview with VirtusizeWebView

--- libsource/src/main/java/com/virtusize/libsource/VirtusizeWebView.kt
+++ libsource/src/main/java/com/virtusize/libsource/VirtusizeWebView.kt
@@ -3,10 +3,12 @@ package com.virtusize.libsource
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Message
 import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import android.webkit.*
 
 @SuppressLint("SetJavaScriptEnabled")
@@ -67,7 +69,7 @@ class VirtusizeWebView @JvmOverloads constructor(
                             return false
                         }
                     }
-                    popupWebView.webChromeClient = object : WebChromeClient(){
+                    popupWebView.webChromeClient = object : WebChromeClient() {
                         override fun onCloseWindow(window: WebView) {
                             window.removeAllViews()
                         }
@@ -79,6 +81,169 @@ class VirtusizeWebView @JvmOverloads constructor(
                     return true
                 }
                 return _webChromeClient?.onCreateWindow(view, dialog, userGesture, resultMsg) ?: false
+            }
+
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                _webChromeClient?.onProgressChanged(view, newProgress)
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                _webChromeClient?.onReceivedTitle(view, title)
+            }
+
+            override fun onReceivedIcon(view: WebView?, icon: Bitmap?) {
+                _webChromeClient?.onReceivedIcon(view, icon)
+            }
+
+            override fun onReceivedTouchIconUrl(
+                view: WebView?,
+                url: String?,
+                precomposed: Boolean
+            ) {
+                _webChromeClient?.onReceivedTouchIconUrl(view, url, precomposed)
+            }
+
+            override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+                _webChromeClient?.onShowCustomView(view, callback)
+            }
+
+            override fun onHideCustomView() {
+                _webChromeClient?.onHideCustomView()
+            }
+
+            override fun onRequestFocus(view: WebView?) {
+                _webChromeClient?.onRequestFocus(view)
+            }
+
+            override fun onCloseWindow(window: WebView?) {
+                _webChromeClient?.onCloseWindow(window)
+            }
+
+            override fun onJsAlert(
+                view: WebView?,
+                url: String?,
+                message: String?,
+                result: JsResult?
+            ): Boolean {
+                return _webChromeClient?.onJsAlert(view, url, message, result) ?: false
+            }
+
+            override fun onJsConfirm(
+                view: WebView?,
+                url: String?,
+                message: String?,
+                result: JsResult?
+            ): Boolean {
+                return _webChromeClient?.onJsConfirm(view, url, message, result) ?: false
+            }
+
+            override fun onJsPrompt(
+                view: WebView?,
+                url: String?,
+                message: String?,
+                defaultValue: String?,
+                result: JsPromptResult?
+            ): Boolean {
+                return _webChromeClient?.onJsPrompt(view, url, message, defaultValue, result)
+                    ?: false
+            }
+
+            override fun onJsBeforeUnload(
+                view: WebView?,
+                url: String?,
+                message: String?,
+                result: JsResult?
+            ): Boolean {
+                return _webChromeClient?.onJsBeforeUnload(view, url, message, result) ?: false
+            }
+
+            override fun onExceededDatabaseQuota(
+                url: String?,
+                databaseIdentifier: String?,
+                quota: Long,
+                estimatedDatabaseSize: Long,
+                totalQuota: Long,
+                quotaUpdater: WebStorage.QuotaUpdater?
+            ) {
+                _webChromeClient?.onExceededDatabaseQuota(
+                    url,
+                    databaseIdentifier,
+                    quota,
+                    estimatedDatabaseSize,
+                    totalQuota,
+                    quotaUpdater
+                )
+            }
+
+            override fun onReachedMaxAppCacheSize(
+                requiredStorage: Long,
+                quota: Long,
+                quotaUpdater: WebStorage.QuotaUpdater?
+            ) {
+                _webChromeClient?.onReachedMaxAppCacheSize(requiredStorage, quota, quotaUpdater)
+            }
+
+            override fun onGeolocationPermissionsShowPrompt(
+                origin: String?,
+                callback: GeolocationPermissions.Callback?
+            ) {
+                _webChromeClient?.onGeolocationPermissionsShowPrompt(origin, callback)
+            }
+
+            override fun onGeolocationPermissionsHidePrompt() {
+                _webChromeClient?.onGeolocationPermissionsHidePrompt()
+            }
+
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                _webChromeClient?.onPermissionRequest(request)
+            }
+
+            override fun onPermissionRequestCanceled(request: PermissionRequest?) {
+                _webChromeClient?.onPermissionRequestCanceled(request)
+            }
+
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+                return _webChromeClient?.onConsoleMessage(consoleMessage) ?: false
+            }
+
+            override fun onJsTimeout(): Boolean {
+                return _webChromeClient?.onJsTimeout() ?: true
+            }
+
+            override fun onConsoleMessage(message: String?, lineNumber: Int, sourceID: String?) {
+                _webChromeClient?.onConsoleMessage(message, lineNumber, sourceID)
+            }
+
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                return _webChromeClient?.onShowFileChooser(
+                    webView,
+                    filePathCallback,
+                    fileChooserParams
+                ) ?: false
+            }
+
+            override fun onShowCustomView(
+                view: View?,
+                requestedOrientation: Int,
+                callback: CustomViewCallback?
+            ) {
+                _webChromeClient?.onShowCustomView(view, requestedOrientation, callback)
+            }
+
+            override fun getDefaultVideoPoster(): Bitmap? {
+                return _webChromeClient?.defaultVideoPoster
+            }
+
+            override fun getVideoLoadingProgressView(): View? {
+                return _webChromeClient?.videoLoadingProgressView
+            }
+
+            override fun getVisitedHistory(callback: ValueCallback<Array<String>>?) {
+                _webChromeClient?.getVisitedHistory(callback)
             }
         })
     }
