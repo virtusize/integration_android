4a489d37ce2e2ad140eaf9789e245220e5440fc2
Kuei <akueisara39@gmail.com>
2021-07-21 14:17:50 +0900

    Add the `product` variable  to the `BodyProfileRecommendedSize` class

--- libsource/src/main/java/com/virtusize/libsource/data/remote/BodyProfileRecommendedSize.kt
+++ libsource/src/main/java/com/virtusize/libsource/data/remote/BodyProfileRecommendedSize.kt
@@ -2,8 +2,10 @@ package com.virtusize.libsource.data.remote
 
 /**
  * This class represents the response for the recommendation API based on the user body profile
+ * @param product the store product that is associated with this recommendation
  * @param sizeName the recommended size name
  */
 data class BodyProfileRecommendedSize(
+    val product: Product,
     val sizeName: String
 )
