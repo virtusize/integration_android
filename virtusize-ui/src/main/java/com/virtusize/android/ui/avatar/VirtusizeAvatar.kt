8ce14dbcd8789eee595777f109d89da03a478823
Kuei <akueisara39@gmail.com>
2021-09-28 10:54:03 +0900

    Add the default `VirtusizeAvatar` component * Move drawables from the virtusize module to virtusize-ui * Add the cardview depedency

---
+++ virtusize-ui/src/main/java/com/virtusize/android/ui/avatar/VirtusizeAvatar.kt
@@ -0,0 +1,18 @@
+package com.virtusize.android.ui.avatar
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.virtusize.android.ui.databinding.VirtusizeAvatarBinding
+
+class VirtusizeAvatar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val binding = VirtusizeAvatarBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+        orientation = VERTICAL
+    }
+}
