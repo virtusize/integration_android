95ebb43a48e6647c98368798d3a9611a4bc0f6f9
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-08-07 23:10:15 +0900

    Fix ktlint error

--- virtusize-ui/src/main/java/com/virtusize/android/ui/avatar/VirtusizeAvatarSize.kt
+++ virtusize-ui/src/main/java/com/virtusize/android/ui/avatar/VirtusizeAvatarSize.kt
@@ -7,28 +7,28 @@ enum class VirtusizeAvatarSize {
     SMALLER {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_smaller_size)
-        };
+        }
     },
     SMALL {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_small_size)
-        };
+        }
     },
     MEDIUM {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_medium_size)
-        };
+        }
     },
     LARGE {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_large_size)
-        };
+        }
     },
     FITTING_ROOM {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_fitting_room_size)
-        };
-    },;
+        }
+    }, ;
 
     abstract fun getAvatarSize(context: Context): Float
 
