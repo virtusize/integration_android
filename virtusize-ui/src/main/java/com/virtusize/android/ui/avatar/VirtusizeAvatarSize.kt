fe7c60027edabe0f8e80154351d605133359050b
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-08-07 23:09:30 +0900

    Reuse the getAvatarSize function for getAvatarRadiusSize

--- virtusize-ui/src/main/java/com/virtusize/android/ui/avatar/VirtusizeAvatarSize.kt
+++ virtusize-ui/src/main/java/com/virtusize/android/ui/avatar/VirtusizeAvatarSize.kt
@@ -7,50 +7,30 @@ enum class VirtusizeAvatarSize {
     SMALLER {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_smaller_size)
-        }
-
-        override fun getAvatarRadiusSize(context: Context): Float {
-            return context.resources.getDimension(R.dimen.vs_avatar_smaller_size) / 2
-        }
+        };
     },
     SMALL {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_small_size)
-        }
-
-        override fun getAvatarRadiusSize(context: Context): Float {
-            return context.resources.getDimension(R.dimen.vs_avatar_small_size) / 2
-        }
+        };
     },
     MEDIUM {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_medium_size)
-        }
-
-        override fun getAvatarRadiusSize(context: Context): Float {
-            return context.resources.getDimension(R.dimen.vs_avatar_medium_size) / 2
-        }
+        };
     },
     LARGE {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_large_size)
-        }
-
-        override fun getAvatarRadiusSize(context: Context): Float {
-            return context.resources.getDimension(R.dimen.vs_avatar_large_size) / 2
-        }
+        };
     },
     FITTING_ROOM {
         override fun getAvatarSize(context: Context): Float {
             return context.resources.getDimension(R.dimen.vs_avatar_fitting_room_size)
-        }
-
-        override fun getAvatarRadiusSize(context: Context): Float {
-            return context.resources.getDimension(R.dimen.vs_avatar_fitting_room_size) / 2
-        }
-    }, ;
+        };
+    },;
 
     abstract fun getAvatarSize(context: Context): Float
 
-    abstract fun getAvatarRadiusSize(context: Context): Float
+    fun getAvatarRadiusSize(context: Context): Float = getAvatarSize(context) / 2
 }
