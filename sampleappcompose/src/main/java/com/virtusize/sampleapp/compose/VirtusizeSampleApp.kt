a9f54a073755ebb8646f66871a6c62e5da62002d
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-08-11 01:01:35 +0900

    Adjust the VirtusizeSampleApp layout

--- sampleappcompose/src/main/java/com/virtusize/sampleapp/compose/VirtusizeSampleApp.kt
+++ sampleappcompose/src/main/java/com/virtusize/sampleapp/compose/VirtusizeSampleApp.kt
@@ -1,14 +1,17 @@
 package com.virtusize.sampleapp.compose
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.virtusize.android.compose.ui.VirtusizeButton
@@ -17,7 +20,11 @@ import com.virtusize.android.data.local.VirtusizeProduct
 @Composable
 internal fun VirtusizeSampleApp() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             val product by remember {
                 mutableStateOf(
                     VirtusizeProduct(
@@ -29,6 +36,7 @@ internal fun VirtusizeSampleApp() {
 
             VirtusizeButton(
                 product = product,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
                 onEvent = { event ->
                     Log.i(VIRTUSIZE_BUTTON_TAG, event.name)
                 },
