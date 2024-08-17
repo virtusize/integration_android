9d95bebf85cf71c173201ec823fba8835e8c0dc2
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-08-17 15:37:50 +0900

    Add preview

--- virtusize/src/main/java/com/virtusize/android/compose/ui/VirtusizeInPageMini.kt
+++ virtusize/src/main/java/com/virtusize/android/compose/ui/VirtusizeInPageMini.kt
@@ -1,17 +1,24 @@
 package com.virtusize.android.compose.ui
 
+import android.view.View
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.viewinterop.NoOpUpdate
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.virtusize.android.compose.theme.VirtusizeColors
 import com.virtusize.android.data.local.VirtusizeError
 import com.virtusize.android.data.local.VirtusizeEvent
 import com.virtusize.android.data.local.VirtusizeProduct
+import com.virtusize.android.data.local.VirtusizeViewStyle
 import com.virtusize.android.model.VirtusizeMessage
 import com.virtusize.android.ui.VirtusizeInPageMini
 import kotlinx.coroutines.flow.launchIn
@@ -35,9 +42,8 @@ fun VirtusizeInPageMini(
 ) {
     val viewModel: VirtusizeComposeViewModel = viewModel<VirtusizeComposeViewModel>()
     val coroutineScope = rememberCoroutineScope()
-    AndroidView(
+    VirtusizeInPageMini(
         modifier = modifier,
-        factory = { context -> VirtusizeInPageMini(context) },
         update = { virtusizeInPageMini ->
             viewModel.isLoadedFlow
                 .onEach { isLoaded ->
@@ -57,5 +63,36 @@ fun VirtusizeInPageMini(
                 is VirtusizeMessage.Error -> onError(message.error)
             }
         }
+    }
+}
+
+@Composable
+private fun VirtusizeInPageMini(
+    modifier: Modifier = Modifier,
+    update: (VirtusizeInPageMini) -> Unit = NoOpUpdate,
+) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context -> VirtusizeInPageMini(context) },
+        update = update,
+    )
+}
+
+@Preview
+@Composable
+fun VirtusizeInPageMiniPreview() {
+    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        VirtusizeInPageMini(
+            update = { virtusizeInPageMini ->
+                virtusizeInPageMini.visibility = View.VISIBLE
+                virtusizeInPageMini.virtusizeViewStyle = VirtusizeViewStyle.TEAL
+            },
+        )
+        VirtusizeInPageMini(
+            update = { virtusizeInPageMini ->
+                virtusizeInPageMini.visibility = View.VISIBLE
+                virtusizeInPageMini.virtusizeViewStyle = VirtusizeViewStyle.BLACK
+            },
+        )
     }
 }
