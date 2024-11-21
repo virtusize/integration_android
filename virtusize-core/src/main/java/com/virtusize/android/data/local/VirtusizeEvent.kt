951f96ac5e1b3cdb766aee9432bd68f1c13da348
Kuei-Jung Hu <kueijunghu@gmail.com>
2024-11-21 23:54:44 +0900

    Move the mapper function to the VirtusizeEventJsonParser file

--- virtusize-core/src/main/java/com/virtusize/android/data/local/VirtusizeEvent.kt
+++ virtusize-core/src/main/java/com/virtusize/android/data/local/VirtusizeEvent.kt
@@ -1,20 +1,6 @@
 package com.virtusize.android.data.local
 
 import androidx.annotation.Keep
-import com.virtusize.android.data.local.VirtusizeEvent.Undefined
-import com.virtusize.android.data.local.VirtusizeEvent.UserAddedProduct
-import com.virtusize.android.data.local.VirtusizeEvent.UserAuthData
-import com.virtusize.android.data.local.VirtusizeEvent.UserChangedRecommendationType
-import com.virtusize.android.data.local.VirtusizeEvent.UserCreatedSilhouette
-import com.virtusize.android.data.local.VirtusizeEvent.UserDeletedData
-import com.virtusize.android.data.local.VirtusizeEvent.UserDeletedProduct
-import com.virtusize.android.data.local.VirtusizeEvent.UserLoggedIn
-import com.virtusize.android.data.local.VirtusizeEvent.UserLoggedOut
-import com.virtusize.android.data.local.VirtusizeEvent.UserOpenedWidget
-import com.virtusize.android.data.local.VirtusizeEvent.UserSawProduct
-import com.virtusize.android.data.local.VirtusizeEvent.UserSawWidgetButton
-import com.virtusize.android.data.local.VirtusizeEvent.UserSelectedProduct
-import com.virtusize.android.data.local.VirtusizeEvent.UserUpdatedBodyMeasurements
 import org.json.JSONObject
 
 /**
@@ -37,7 +23,7 @@ enum class EventName(val value: String) {
     ;
 
     companion object {
-        fun valueOf(value: String): EventName? = entries.find { it.value == value }
+        fun get(value: String): EventName? = entries.find { it.value == value }
     }
 }
 
@@ -105,25 +91,6 @@ sealed interface VirtusizeEvent {
 
     data class Undefined(override val name: String, override val data: JSONObject? = null) : VirtusizeEvent
 }
-
-fun EventName.toVirtusizeEvent(json: JSONObject): VirtusizeEvent =
-    EventName.entries.find { entry -> entry.value == name }?.let { entry ->
-        when (entry) {
-            EventName.UserSawProduct -> UserSawProduct(data = json)
-            EventName.UserSawWidgetButton -> UserSawWidgetButton(data = json)
-            EventName.UserOpenedWidget -> UserOpenedWidget(data = json)
-            EventName.UserSelectedProduct -> UserSelectedProduct(data = json)
-            EventName.UserAddedProduct -> UserAddedProduct(data = json)
-            EventName.UserDeletedProduct -> UserDeletedProduct(data = json)
-            EventName.UserChangedRecommendationType -> UserChangedRecommendationType(data = json)
-            EventName.UserCreatedSilhouette -> UserCreatedSilhouette(data = json)
-            EventName.UserUpdatedBodyMeasurements -> UserUpdatedBodyMeasurements(data = json)
-            EventName.UserAuthData -> UserAuthData(data = json)
-            EventName.UserLoggedIn -> UserLoggedIn(data = json)
-            EventName.UserLoggedOut -> UserLoggedOut(data = json)
-            EventName.UserDeletedData -> UserDeletedData(data = json)
-        }
-    } ?: Undefined(name = name, data = json)
 
 /**
  * This enum contains the size comparison types Virtusize provides
--- virtusize/src/main/java/com/virtusize/android/data/parsers/VirtusizeEventJsonParser.kt
+++ virtusize/src/main/java/com/virtusize/android/data/parsers/VirtusizeEventJsonParser.kt
@@ -2,7 +2,20 @@ package com.virtusize.android.data.parsers
 
 import com.virtusize.android.data.local.EventName
 import com.virtusize.android.data.local.VirtusizeEvent
-import com.virtusize.android.data.local.toVirtusizeEvent
+import com.virtusize.android.data.local.VirtusizeEvent.Undefined
+import com.virtusize.android.data.local.VirtusizeEvent.UserAddedProduct
+import com.virtusize.android.data.local.VirtusizeEvent.UserAuthData
+import com.virtusize.android.data.local.VirtusizeEvent.UserChangedRecommendationType
+import com.virtusize.android.data.local.VirtusizeEvent.UserCreatedSilhouette
+import com.virtusize.android.data.local.VirtusizeEvent.UserDeletedData
+import com.virtusize.android.data.local.VirtusizeEvent.UserDeletedProduct
+import com.virtusize.android.data.local.VirtusizeEvent.UserLoggedIn
+import com.virtusize.android.data.local.VirtusizeEvent.UserLoggedOut
+import com.virtusize.android.data.local.VirtusizeEvent.UserOpenedWidget
+import com.virtusize.android.data.local.VirtusizeEvent.UserSawProduct
+import com.virtusize.android.data.local.VirtusizeEvent.UserSawWidgetButton
+import com.virtusize.android.data.local.VirtusizeEvent.UserSelectedProduct
+import com.virtusize.android.data.local.VirtusizeEvent.UserUpdatedBodyMeasurements
 import org.json.JSONObject
 
 /**
@@ -11,11 +24,27 @@ import org.json.JSONObject
 internal class VirtusizeEventJsonParser : VirtusizeJsonParser<VirtusizeEvent> {
     override fun parse(json: JSONObject): VirtusizeEvent? {
         val name = JsonUtils.optNullableString(json, FIELD_NAME) ?: return null
-        val eventName = EventName.valueOf(name)
-        return eventName.toVirtusizeEvent(json)
+        return getVirtusizeEvent(name = name, json = json)
     }
 
     companion object {
         private const val FIELD_NAME = "eventName"
     }
+}
+
+private fun getVirtusizeEvent(name: String, json: JSONObject): VirtusizeEvent = when (EventName.get(name)) {
+    EventName.UserSawProduct -> UserSawProduct(data = json)
+    EventName.UserSawWidgetButton -> UserSawWidgetButton(data = json)
+    EventName.UserOpenedWidget -> UserOpenedWidget(data = json)
+    EventName.UserSelectedProduct -> UserSelectedProduct(data = json)
+    EventName.UserAddedProduct -> UserAddedProduct(data = json)
+    EventName.UserDeletedProduct -> UserDeletedProduct(data = json)
+    EventName.UserChangedRecommendationType -> UserChangedRecommendationType(data = json)
+    EventName.UserCreatedSilhouette -> UserCreatedSilhouette(data = json)
+    EventName.UserUpdatedBodyMeasurements -> UserUpdatedBodyMeasurements(data = json)
+    EventName.UserAuthData -> UserAuthData(data = json)
+    EventName.UserLoggedIn -> UserLoggedIn(data = json)
+    EventName.UserLoggedOut -> UserLoggedOut(data = json)
+    EventName.UserDeletedData -> UserDeletedData(data = json)
+    null -> Undefined(name = name, data = json)
 }
