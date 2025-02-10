acb17f88c7654a2b3655c294124f4b6b885afe7f
Alexey Shikov <alexey.shikov@gmail.com>
2025-02-10 11:48:46 +0100

    Use kotlin syntax

--- virtusize-core/src/main/java/com/virtusize/android/network/VirtusizeEndpoint.kt
+++ virtusize-core/src/main/java/com/virtusize/android/network/VirtusizeEndpoint.kt
@@ -20,18 +20,18 @@ sealed interface VirtusizeEndpoint {
 
     data class VirtusizeWebView(val version: String, val branch: String?) : VirtusizeEndpoint {
         override val path: String =
-            when {
-                branch == null -> "/a/aoyama/$version/sdk-webview.html"
-                branch == "staging" -> "/a/aoyama/staging/sdk-webview.html"
+            when (branch) {
+                null -> "/a/aoyama/$version/sdk-webview.html"
+                "staging" -> "/a/aoyama/staging/sdk-webview.html"
                 else -> "/a/aoyama/testing/$branch/sdk-webview.html"
             }
     }
 
     data class VirtusizeWebViewForSpecificClients(val branch: String?) : VirtusizeEndpoint {
         override val path: String =
-            when {
-                branch == null -> "/a/aoyama/testing/privacy-policy-phase2-vue/sdk-webview.html"
-                branch == "staging" -> "/a/aoyama/staging/sdk-webview.html"
+            when (branch) {
+                null -> "/a/aoyama/testing/privacy-policy-phase2-vue/sdk-webview.html"
+                "staging" -> "/a/aoyama/staging/sdk-webview.html"
                 else -> "/a/aoyama/testing/$branch/sdk-webview.html"
             }
     }
