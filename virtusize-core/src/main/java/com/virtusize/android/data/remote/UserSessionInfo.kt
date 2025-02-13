6b7cc9d29d927a4c0a052082fec197a2317b9f90
Alexey Shikov <alexey.shikov@gmail.com>
2025-02-13 15:04:29 +0100

    Load body measurements based on user-session flag

--- virtusize-core/src/main/java/com/virtusize/android/data/remote/UserSessionInfo.kt
+++ virtusize-core/src/main/java/com/virtusize/android/data/remote/UserSessionInfo.kt
@@ -6,10 +6,12 @@ package com.virtusize.android.data.remote
  * @param bid the browser ID
  * @param authToken the auth token
  * @param userSessionResponse the API response as a string
+ * @param hasBodyMeasurement the flag to indicate if a user specified body measurements
  */
 data class UserSessionInfo(
     val accessToken: String,
     internal val bid: String?,
     val authToken: String,
+    val hasBodyMeasurement: Boolean,
     val userSessionResponse: String,
 )
