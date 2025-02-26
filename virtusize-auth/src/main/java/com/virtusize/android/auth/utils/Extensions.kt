cc44cf00ba513d3041bffa87fd32639736a1e909
Alexey Shikov <alexey.shikov@gmail.com>
2025-02-26 14:22:40 +0100

    ktlint

--- virtusize-auth/src/main/java/com/virtusize/android/auth/utils/Extensions.kt
+++ virtusize-auth/src/main/java/com/virtusize/android/auth/utils/Extensions.kt
@@ -7,5 +7,4 @@ internal fun String.isVirtusizeSNSAuthURL(): Boolean = matches(regex = Regex("ht
 internal fun String.isVirtusizeAuthAppURL(): Boolean =
     matches(regex = Regex("https://api\\.virtusize\\.\\w+/auth/\\w+/index\\.html(\\?.*)?"))
 
-fun Uri.withBranch(branchName: String): Uri =
-    VirtusizeUriHelper.updateUriParameter(this, "vs-branch", branchName)
+fun Uri.withBranch(branchName: String): Uri = VirtusizeUriHelper.updateUriParameter(this, "vs-branch", branchName)
